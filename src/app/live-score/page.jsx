405f82f63cab58cf3c6897a6cabd5f29c06b9a7f
dimitargluharski <dimitargluharski@gmail.com>
2023-06-27 20:51:08 +0300

    MatchList component fetch SSR and revalidates

--- src/app/live-score/page.jsx
+++ src/app/live-score/page.jsx
@@ -1,54 +1,35 @@
-"use client";
-
-import { useState, useEffect } from "react";
 import Row from "@/components/Row/Row";
-import SearchField from "@/components/Search/SearchField";
 
-const url = "https://v3.football.api-sports.io/fixtures?live=all";
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "64fa06bb722e21b379fa99db2e95e3bf",
-    "X-RapidAPI-Host": "v3.football.api-sports.io",
-  },
-};
+async function getData() {
+  const response = await fetch(
+    "https://v3.football.api-sports.io/fixtures?live=all",
+    {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "64fa06bb722e21b379fa99db2e95e3bf",
+        "X-RapidAPI-Host": "v3.football.api-sports.io",
+      },
+      next: {
+        revalidate: 300, // 5min
+      },
+    }
+  );
 
-const MatchesList = () => {
-  const [matches, setMatches] = useState([]);
-  const [filteredMatches, setFilteredMatches] = useState([]);
+  const data = await response.json();
 
-  useEffect(() => {
-    fetch(url, options)
-      .then((res) => res.json())
-      .then((data) => {
-        setMatches(data.response);
-      })
-      .catch((err) => console.log(err));
+  return data.response;
+}
 
-    // Cleanup
-    return () => {};
-  }, []);
-
-  const handleSearch = (searchTerm) => {
-    const filtered = matches.filter(
-      (match) =>
-        match.teams.home.name
-          .toLowerCase()
-          .includes(searchTerm.toLowerCase()) ||
-        match.teams.away.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-
-    setFilteredMatches(filtered);
-  };
+const MatchList = async () => {
+  const data = await getData();
 
   return (
-    <div>
-      <SearchField onSearch={handleSearch} />
-      {(filteredMatches.length > 0 ? filteredMatches : matches).map((match) => (
-        <Row {...match} key={match.fixture.id} />
+    <>
+      {data.map((match) => (
+        <Row key={match.fixture.id} {...match} />
       ))}
-    </div>
+    </>
   );
 };
 
-export default MatchesList;
+export default MatchList;
